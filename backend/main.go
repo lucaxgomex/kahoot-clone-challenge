c99d86e1898192fe22ebf561b34a7ebeb4bc77e5
lucaxgomex <lg.projectdev@gmail.com>
2025-09-22 17:39:22 -0300

    dev: it was defined struct inside main code and functions parameters

--- backend/main.go
+++ backend/main.go
@@ -1,13 +1,24 @@
 package main
 
 import (
-    "log"
+	"log"
 
-    "github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2"
+
+	"context"
+	"time"
+
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
 )
+
+var quizColletion *mongo.Collection
 
 func main() {
     app := fiber.New()
+
+	setupDb()
 
 	app.Get("/", index)
     app.Get("/api/quizzes", getQuizzes)
@@ -15,16 +26,36 @@ func main() {
     log.Fatal(app.Listen(":3000"))
 }
 
+func setupDb() {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	
+	defer cancel()
+
+	client, err := mongo.Connect(ctx,
+		options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		panic(err)
+	}
+
+	quizColletion = client.Database("quiz").Collection("quizzes")
+}
+
 func index(c *fiber.Ctx) error {
 	return c.SendString("Hello World!")
 }
 
 func getQuizzes(c *fiber.Ctx) error {
-	list := []map[string]any{
-		{	
-			"teste": 123,
-dsd		},
+	cursor, err := quizColletion.Find(context.Background(), bson.M{})
+	if err != nil {
+		panic(err)
 	}
 
-	return c.JSON(list)
+	quizzes := 	[]map[string]any{}
+
+	err = cursor.All(context.Background(), &quizzes)
+	if err != nil {
+		panic(err)
+	}
+
+	return c.JSON(quizzes)
 }
